222d39ba4465c2dde37d55d81eb29ae1ae472157
olcay-elt <olcay.elt@gmail.com>
2023-04-18 09:22:09 +0200

    Add

--- class-notes/02_OOP/OOP_Inclass.py
+++ class-notes/02_OOP/OOP_Inclass.py
@@ -4,7 +4,16 @@ os.system('cls' if os.name == 'nt' else 'clear')
 print("-------------------------------------")
 
 
+def print_types(data):
+    for i in data:
+        print(i, type(i))
+        
+test=[122, "victor", ]
 
+
+class Person:
+    name="victor"
+    age=18
 
 
 
