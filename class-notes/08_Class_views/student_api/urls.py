2eae10d64a4322100b3a458cd377c006a929f537
olcay-elt <olcay.elt@gmail.com>
2023-04-28 09:53:00 +0200

    Add

--- class-notes/08_Class_views/student_api/urls.py
+++ class-notes/08_Class_views/student_api/urls.py
@@ -1,19 +1,33 @@
 from django.urls import path
 from .views import (
     home, 
+    student_api, student_api_get_update_delete,
     student_api, 
     student_create, 
     student_detail, 
     student_update,
-    student_delete
+    student_delete,
+    StudentListCreate,
+    StudentDetail
     )
 
 urlpatterns = [
     path('', home),
-    path("student-list/", student_api, name="list"),
-    path("student-create/", student_create, name="create"),
-    path("student-detail/<int:pk>", student_detail, name="detail"),
-    path("student-update/<int:pk>", student_update, name="update"),
-    path("student-delete/<int:pk>", student_delete, name="delete"),
+    
+    #! Func endpoints
+    # path('student/', student_api),
+    # path('student/<int:pk>/', student_api_get_update_delete, name = "detail"),
+    
+    # path("student-list/", student_api, name="list"),
+    # path("student-create/", student_create, name="create"),
+    # path("student-detail/<int:pk>", student_detail, name="detail"),
+    # path("student-update/<int:pk>", student_update, name="update"),
+    # path("student-delete/<int:pk>", student_delete, name="delete"),
+    
+    #! Class endpoints
+    path("student/", StudentListCreate.as_view()),
+    path("student/<int:pk>", StudentDetail.as_view()),
 ]
+
+
 
