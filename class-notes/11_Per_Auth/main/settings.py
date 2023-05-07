1a0667d7bddd1b772da84f1b9f8b756ee8b4fe57
olcay-elt <olcay.elt@gmail.com>
2023-05-07 18:40:59 +0200

    Add

--- class-notes/11_Per_Auth/main/settings.py
+++ class-notes/11_Per_Auth/main/settings.py
@@ -46,7 +46,16 @@ INSTALLED_APPS = [
     # 'django_filters',
 ]
 
-
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
 
 ROOT_URLCONF = 'main.urls'
 
