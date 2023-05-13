dbe4632053856a2de3a415e218cbed24f70610ab
olcay-elt <olcay.elt@gmail.com>
2023-05-13 23:12:30 +0200

    Add

--- class-notes/13_Flight_App_Setup/main/settings/dev.py
+++ class-notes/13_Flight_App_Setup/main/settings/dev.py
@@ -10,7 +10,14 @@ THIRD_PARTY_MIDDLEWARE = ["debug_toolbar.middleware.DebugToolbarMiddleware"]
  
 MIDDLEWARE += THIRD_PARTY_MIDDLEWARE 
  
-
+# Database
+# https://docs.djangoproject.com/en/4.0/ref/settings/#databases 
+DATABASES = { 
+    "default": { 
+        "ENGINE": "django.db.backends.sqlite3", 
+        "NAME": BASE_DIR / "db.sqlite3", 
+    } 
+} 
  
 INTERNAL_IPS = [ 
     "127.0.0.1", 
