e4e38a0f7da7d19462de9413e192c04740e8462b
olcay-elt <olcay.elt@gmail.com>
2023-05-13 23:12:52 +0200

    Add

--- class-notes/13_Flight_App_Setup/main/settings/dev.py
+++ class-notes/13_Flight_App_Setup/main/settings/dev.py
@@ -6,9 +6,7 @@ DEBUG = True
  
 INSTALLED_APPS += THIRD_PARTY_APPS 
  
-THIRD_PARTY_MIDDLEWARE = ["debug_toolbar.middleware.DebugToolbarMiddleware"] 
- 
-MIDDLEWARE += THIRD_PARTY_MIDDLEWARE 
+
  
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases 
