833836e72287cafbe34685caaae77d68bc8ae0b4
olcay-elt <olcay.elt@gmail.com>
2023-05-15 09:36:51 +0200

    Add

---
+++ class-notes/14_Flight_App_Auth/users/urls.py
@@ -0,0 +1,21 @@
+from django.urls import path, include
+from .views import RegisterView
+
+
+# localhost8000/users/auth/... 
+urlpatterns = [
+    path('auth/', include('dj_rest_auth.urls')),
+    path("register/", RegisterView.as_view())
+]
+
+
+
+
+  
+  
+  # login / logout ------->> dj-rest-auth
+  # register   ------->> (url - view - serializer)
+  
+  # generate token with signal return token in registerview
+  # return user data in login response
+  
