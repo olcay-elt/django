69d9270505e13741c71b998a7b927e817abed8a2
olcay-elt <olcay.elt@gmail.com>
2023-05-28 22:48:01 +0200

    Add

--- class-notes/15_Flight_App/flight/models.py
+++ class-notes/15_Flight_App/flight/models.py
@@ -13,13 +13,6 @@ class Flight(models.Model):
     def __str__(self):
         return f'{self.flight_number} - {self.departure_city} - {self.arrival_city}'
     
-    
-class Passenger(models.Model):
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=40)
-    email = models.EmailField()
-    phone_number = models.IntegerField()
-    create_date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f'{self.first_name} {self.last_name}'
