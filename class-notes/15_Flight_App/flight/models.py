b154672e17e1f1302db12ef42f6b45bfb0a5b2ab
olcay-elt <olcay.elt@gmail.com>
2023-05-28 22:47:37 +0200

    Add

--- class-notes/15_Flight_App/flight/models.py
+++ class-notes/15_Flight_App/flight/models.py
@@ -2,13 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-class Flight(models.Model):
-    flight_number = models.CharField(max_length=10)
-    operation_airlines = models.CharField(max_length=15)
-    departure_city = models.CharField(max_length=30)
-    arrival_city = models.CharField(max_length=30)
-    date_of_departure = models.DateField()
-    etd = models.TimeField()
+
 
     def __str__(self):
         return f'{self.flight_number} - {self.departure_city} - {self.arrival_city}'
