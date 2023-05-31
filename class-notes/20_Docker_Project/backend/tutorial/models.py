047bb5ec2d855fe5b77d7a90209fd84f74497045
olcay-elt <olcay.elt@gmail.com>
2023-05-31 21:26:57 +0200

    Add

--- class-notes/20_Docker_Project/backend/tutorial/models.py
+++ class-notes/20_Docker_Project/backend/tutorial/models.py
@@ -1,7 +1,9 @@
 from django.db import models
 
 # Create your models here.
-
+class Tutorial(models.Model):
+    title= models.CharField(max_length=100)
+    description = models.TextField()
 
     
 
