c0ea0979188eb62af0f9bf0fa3b6ffb4557e34ad
olcay-elt <olcay.elt@gmail.com>
2023-05-31 21:28:23 +0200

    Add

--- class-notes/15_Flight_App/flight/models.py
+++ class-notes/15_Flight_App/flight/models.py
@@ -21,8 +21,7 @@ class Passenger(models.Model):
     create_date = models.DateTimeField(auto_now_add=True)
 
 
-    def __str__(self):
-        return f'{self.first_name} {self.last_name}'
+
     
 
 class Reservation(models.Model):
